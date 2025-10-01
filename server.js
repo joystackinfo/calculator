232b6d4b730fb34976fb11d458c6ba15e9b16ee4
joystackinfo <joynelsoninfo@gmail.com>
2025-10-01 15:03:44 +0100

    added server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const math = require("mathjs");
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// serve frontend files from public/
+app.use(express.static("public"));
+
+// one endpoint for all operations
+app.post("/calculate", (req, res) => {
+  try {
+    const { expression } = req.body; // get expression from frontend
+    const result = math.evaluate(expression); // math.js evaluates safely
+    res.json({ result });
+  } catch (err) {
+    res.json({ error: "Invalid Expression" });
+  }
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
